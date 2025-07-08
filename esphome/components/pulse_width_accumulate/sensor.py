8b5ec494aa1325cd189fded85a1f62a295f05062
Lukas Kolbe <lukas@einfachkaffee.de>
2025-07-08 23:23:16 +0200

    add pulse_width_accumulate

---
+++ esphome/components/pulse_width_accumulate/sensor.py
@@ -0,0 +1,79 @@
+from esphome import pins
+import esphome.codegen as cg
+from esphome.components import sensor
+import esphome.config_validation as cv
+from esphome.const import (
+    CONF_FREQUENCY,
+    CONF_PIN,
+    ICON_PULSE,
+    ICON_TIMER,
+    STATE_CLASS_MEASUREMENT,
+    STATE_CLASS_TOTAL_INCREASING,
+    UNIT_HERTZ,
+    UNIT_SECOND,
+)
+
+CONF_RELATIVE = "relative"
+CONF_LOWER_THRESHOLD = "lower_threshold_us"
+
+pulse_width_ns = cg.esphome_ns.namespace("pulse_width_accumulate")
+
+PulseWidthAccumulateSensor = pulse_width_ns.class_(
+    "PulseWidthAccumulateSensor", sensor.Sensor, cg.PollingComponent
+)
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        PulseWidthAccumulateSensor,
+        unit_of_measurement=UNIT_SECOND,
+        icon=ICON_TIMER,
+        accuracy_decimals=2,
+        state_class=STATE_CLASS_TOTAL_INCREASING,
+    )
+    .extend(
+        {
+            cv.Required(CONF_PIN): cv.All(pins.internal_gpio_input_pin_schema),
+            cv.Optional(CONF_FREQUENCY): sensor.sensor_schema(
+                unit_of_measurement=UNIT_HERTZ,
+                accuracy_decimals=1,
+                icon=ICON_PULSE,
+                state_class=STATE_CLASS_MEASUREMENT,
+            ),
+            cv.Optional(CONF_RELATIVE, default=False): cv.boolean,
+            cv.Optional(CONF_LOWER_THRESHOLD, default=17): cv.int_range(17, 10000),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+)
+
+
+async def to_code(config):
+    # Register the main sensor
+    var = await sensor.new_sensor(config)
+    await cg.register_component(var, config)
+    # Configure the GPIO pin
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
+    # Register the frequency sub-sensor if configured
+    if conf_freq := config.get(CONF_FREQUENCY):
+        sens = await sensor.new_sensor(conf_freq)
+        cg.add(var.set_frequency_sensor(sens))
+    # Register the lower threshold if configured
+    if CONF_LOWER_THRESHOLD in config:
+        cg.add_define("LOWER_PULSE_WIDTH_THRESHOLD_VALUE", config[CONF_LOWER_THRESHOLD])
+    # Pass the relative option to the C++ code
+    if config[CONF_RELATIVE]:
+        cg.add(var.set_relative_mode(True))
+        # Only set defaults if the user hasn't overridden them
+        if "unit_of_measurement" not in config:
+            cg.add(var.set_unit_of_measurement("%"))
+        if "icon" not in config:
+            cg.add(var.set_icon(ICON_TIMER))
+        if "accuracy_decimals" not in config:
+            cg.add(var.set_accuracy_decimals(1))
+        if "state_class" not in config:
+            cg.add(
+                var.set_state_class(
+                    cg.RawExpression("esphome::sensor::STATE_CLASS_MEASUREMENT")
+                )
+            )
